6bf76d7886af1c4d32a4089ecba76a6b0c82751e
Felice-Chuang <86038401+Felice-Chuang@users.noreply.github.com>
2021-06-17 00:47:50 -0500

    Update top_Play.sql

--- top_Play.sql
+++ top_Play.sql
@@ -3,9 +3,9 @@ CREATE TABLE IF NOT EXISTS public.top_Play
 (
     "gamePk" bigint,
     "total_pitch" int COLLATE pg_catalog."default",
-	"highest_exit_velocity_playerId" int COLLATE pg_catalog."default",
-	"highest_exit_velocity_playerName" Text COLLATE pg_catalog."default",
-    "highest_exit_velocity" float8 COLLATE pg_catalog."default",
+    "highest_exit_velocity_playerId" int COLLATE pg_catalog."default",
+    "highest_exit_velocity_playerName" Text COLLATE pg_catalog."default",
+    "highest_exit_velocity" float8 COLLATE pg_catalog."default"
     
 )
 
@@ -13,6 +13,6 @@ TABLESPACE pg_default;
 
 
 CREATE INDEX ix_top_Play_index
-    ON public.daily_completed_games_details USING btree
+    ON public.top_Play USING btree
     (index ASC NULLS LAST)
     TABLESPACE pg_default;
